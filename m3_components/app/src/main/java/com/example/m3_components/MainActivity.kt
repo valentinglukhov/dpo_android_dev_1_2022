0bd7c3097d62172c8901f3bf6d26094d391bdda0
Валентин Глухов <glukhov_vy@vk.com>
2023-03-17 16:05:19 +0400

    Done

---
+++ m3_components/app/src/main/java/com/example/m3_components/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.m3_components
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import com.example.m3_components.databinding.ActivityMainBinding
+import com.google.android.material.slider.Slider
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    private var countNumber = 10
+    private var job: Job? = null
+    var isRunning = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sliderBar.addOnChangeListener { _, value, _ ->
+            countNumber = value.toInt()
+            binding.timerNumber.text = value.toInt().toString()
+            binding.progressBar.max = value.toInt()
+            binding.progressBar.progress = value.toInt()
+            isRunning = false
+        }
+
+        binding.actionButton.setOnClickListener {
+            actionTimer(countNumber)
+        }
+    }
+
+
+    private fun actionTimer(countNumber: Int) {
+        val timerNumber = findViewById<TextView>(R.id.timerNumber)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val sliderBar = findViewById<Slider>(R.id.sliderBar)
+        val actionButton = findViewById<Button>(R.id.actionButton)
+//        if (job?.isActive == true) {
+//            job?.cancel()
+//            job = null
+//        }
+        if (!isRunning) {
+            isRunning = true
+            actionButton.setText(R.string.stop_timer)
+            sliderBar.isEnabled = false
+            job = CoroutineScope(Dispatchers.Main).launch {
+                for (i in countNumber downTo 0) {
+                    timerNumber.text = i.toString()
+                    progressBar.progress = i
+                    if (i > 0) delay(1000)
+                }
+                isRunning = false
+                timerNumber.text = sliderBar.value.toInt().toString()
+                progressBar.progress = countNumber
+                actionButton.setText(R.string.start_timer)
+                sliderBar.isEnabled = true
+                Toast.makeText(this@MainActivity, "Таймер завершен", Toast.LENGTH_SHORT).show()
+                cancel()
+//                job = null
+            }
+        } else {
+            job?.cancel()
+            isRunning = false
+            timerNumber.text = sliderBar.value.toInt().toString()
+            progressBar.progress = countNumber
+            actionButton.setText(R.string.start_timer)
+            sliderBar.isEnabled = true
+            Toast.makeText(this@MainActivity, "Таймер остановлен", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+sealed class TimerState {
+    class Stopped : TimerState()
+    class Running : TimerState()
+    class Finished : TimerState()
+}
+
---
+++ m3_components/app/src/main/java/com/example/m3_components/Timer.kt
@@ -0,0 +1,8 @@
+package com.example.m3_components
+
+import androidx.appcompat.app.AppCompatActivity
+
+object Timer {
+    fun start (countNumber: Int) {
+    }
+}
