8d8cb25078812013c3b3493e3d2e22c111e7baf8
Валентин Глухов <glukhov_vy@vk.com>
2023-03-21 12:56:50 +0400

    Алексей, спасибо за помощь =)

--- m3_components/app/src/main/java/com/example/m3_components/MainActivity.kt
+++ m3_components/app/src/main/java/com/example/m3_components/MainActivity.kt
@@ -2,90 +2,91 @@ package com.example.m3_components
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
 import android.widget.Toast
 import com.example.m3_components.databinding.ActivityMainBinding
-import com.google.android.material.slider.Slider
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var countNumber = 10
     private var job: Job? = null
     var isRunning = false
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.sliderBar.addOnChangeListener { _, value, _ ->
-            countNumber = value.toInt()
-            binding.timerNumber.text = value.toInt().toString()
-            binding.progressBar.max = value.toInt()
-            binding.progressBar.progress = value.toInt()
-            isRunning = false
-        }
+        binding.sliderBar.addOnChangeListener { _, _, _ -> updateUI(TimerState.Idle()) }
 
         binding.actionButton.setOnClickListener {
-            getTimerNumber()
             actionTimer(countNumber)
         }
     }
 
-    private fun getTimerNumber() = findViewById<TextView>(R.id.timerNumber)
+    private fun updateUI(state: TimerState, timerValue: Int? = null) {
+        when (state) {
+            is TimerState.Idle -> {
+                countNumber = binding.sliderBar.value.toInt()
+                binding.timerNumber.text = binding.sliderBar.value.toInt().toString()
+                binding.progressBar.max = binding.sliderBar.value.toInt()
+                binding.progressBar.progress = binding.sliderBar.value.toInt()
+                isRunning = false
+            }
+            is TimerState.Run -> {
+                isRunning = true
+                binding.actionButton.setText(R.string.stop_timer)
+                binding.sliderBar.isEnabled = false
+            }
+            is TimerState.Stop -> {
+                isRunning = false
+                binding.timerNumber.text = binding.sliderBar.value.toInt().toString()
+                binding.progressBar.progress = countNumber
+                binding.actionButton.setText(R.string.start_timer)
+                binding.sliderBar.isEnabled = true
+                Toast.makeText(this, "Таймер остановлен", Toast.LENGTH_SHORT).show()
+
+            }
+            is TimerState.Finish -> {
+                isRunning = false
+                binding.timerNumber.text = binding.sliderBar.value.toInt().toString()
+                binding.progressBar.progress = countNumber
+                binding.actionButton.setText(R.string.start_timer)
+                binding.sliderBar.isEnabled = true
+                Toast.makeText(this, "Таймер завершен", Toast.LENGTH_SHORT).show()
+            }
+            is TimerState.UpdateProgress -> {
+                binding.timerNumber.text = timerValue.toString()
+                binding.progressBar.progress = timerValue.toString().toInt()
+            }
+        }
+    }
 
 
     private fun actionTimer(countNumber: Int) {
-        val timerNumber = findViewById<TextView>(R.id.timerNumber)
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val sliderBar = findViewById<Slider>(R.id.sliderBar)
-        val actionButton = findViewById<Button>(R.id.actionButton)
         if (!isRunning) {
-            isRunning = true
-            actionButton.setText(R.string.stop_timer)
-            sliderBar.isEnabled = false
+            updateUI(TimerState.Run())
             job = CoroutineScope(Dispatchers.Main).launch {
-                for (i in countNumber downTo 0) {
-                    timerNumber.text = i.toString()
-                    progressBar.progress = i
-                    if (i > 0) delay(1000)
+                for (timerValue in countNumber downTo 1) {
+                    updateUI(TimerState.UpdateProgress(), timerValue)
+                    delay(1000)
                 }
-                isRunning = false
-                timerNumber.text = sliderBar.value.toInt().toString()
-                progressBar.progress = countNumber
-                actionButton.setText(R.string.start_timer)
-                sliderBar.isEnabled = true
-                Toast.makeText(this@MainActivity, "Таймер завершен", Toast.LENGTH_SHORT).show()
+                updateUI(TimerState.Finish())
                 cancel()
             }
         } else {
             job?.cancel()
-            isRunning = false
-            timerNumber.text = sliderBar.value.toInt().toString()
-            progressBar.progress = countNumber
-            actionButton.setText(R.string.start_timer)
-            sliderBar.isEnabled = true
-            Toast.makeText(this@MainActivity, "Таймер остановлен", Toast.LENGTH_SHORT).show()
+            updateUI(TimerState.Stop())
         }
     }
 
 
     sealed class TimerState {
-        class Stopped : TimerState()
-        class Running : TimerState()
-        class Finished : TimerState()
-
-        fun action (action: TimerState) {
-            when(action) {
-                is Stopped -> {
-
-                }
-
-                else -> {}
-            }
-        }
+        class Stop : TimerState()
+        class Run : TimerState()
+        class Finish : TimerState()
+        class Idle : TimerState()
+        class UpdateProgress : TimerState()
     }
 }
 
