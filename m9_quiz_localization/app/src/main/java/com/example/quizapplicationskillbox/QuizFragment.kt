e640044ddf188095df25aa269121413af0d044ca
Валентин Глухов <glukhov_vy@vk.com>
2023-03-29 01:15:43 +0400

    Практическая работа готова

---
+++ m9_quiz_localization/app/src/main/java/com/example/quizapplicationskillbox/QuizFragment.kt
@@ -0,0 +1,152 @@
+package com.example.quizapplicationskillbox
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.forEach
+import androidx.core.view.setPadding
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.quizapplicationskillbox.databinding.FragmentQuizBinding
+import com.example.quizapplicationskillbox.quiz.QuizStorage
+import kotlinx.coroutines.*
+import java.util.*
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [QuizFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class QuizFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding: FragmentQuizBinding? = null
+    private val binding get() = _binding!!
+    private var results = mutableListOf<Int>()
+    private var radioGroupList: MutableList<RadioGroup> = mutableListOf()
+    private val quiz = when (Locale.getDefault().language) {
+        "en" -> QuizStorage.Locale.En
+        "ru" -> QuizStorage.Locale.Ru
+        else -> {QuizStorage.Locale.En}
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentQuizBinding.inflate(inflater)
+        val bundle = Bundle()
+        radioGroupList = inflateRadioGroup()
+        val job = CoroutineScope(Dispatchers.Main).launch {
+            delay(200)
+            radioGroupList.forEach {
+                it.forEach {
+                    delay(50)
+                    it.animate().apply {
+                        duration = 700
+                        alpha(1F)
+                    }
+                }
+            }
+            cancel()
+        }
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_quizFragment_to_mainFragment)
+        }
+
+        binding.sendButton.setOnClickListener {
+            results = getResultFromRadioGroup(radioGroupList)
+            if (results.size < 3) {
+                Toast.makeText(
+                    this.context,
+                    getString(R.string.please_answer_all_questions),
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            val quizAnswers =
+                QuizStorage.answer(QuizStorage.getQuiz(quiz), results)
+            bundle.putString("quizAnswers", quizAnswers)
+            findNavController().navigate(
+                R.id.action_quizFragment_to_resultsFragment, args = bundle
+            )
+        }
+        return binding.root
+    }
+
+    fun getResultFromRadioGroup(radioGroupList: MutableList<RadioGroup>): MutableList<Int> {
+        val resultList: MutableList<Int> = mutableListOf()
+        radioGroupList.forEachIndexed { _, radioGroup ->
+            if (radioGroup.checkedRadioButtonId >= 0) resultList.add(radioGroup.checkedRadioButtonId)
+        }
+        return resultList
+    }
+
+
+    fun inflateRadioGroup(): MutableList<RadioGroup> {
+        radioGroupList.clear()
+        val paddingDp = 16
+        val paddingPx = (resources.displayMetrics.density * paddingDp + 0.5F).toInt()
+        var radioGroup: RadioGroup
+        val linearLayout = binding.linearLayout
+        val questions = QuizStorage.getQuiz(quiz).questions
+        for (question in questions.indices) {
+            var answerId = 0
+            radioGroup = RadioGroup(this.context)
+            radioGroup.orientation = RadioGroup.VERTICAL
+            val questionText = TextView(this.context)
+            questionText.text = questions[question].question
+            questionText.id = View.generateViewId()
+            questionText.setPadding(paddingPx)
+            questionText.alpha = 0F
+            radioGroup.addView(questionText)
+            for (answer in questions[question].answers.indices) {
+                val radioButton = RadioButton(this.context)
+                radioButton.text = questions[question].answers[answer]
+                radioButton.id = answerId
+                radioButton.alpha = 0F
+                radioGroup.addView(radioButton)
+                answerId++
+            }
+            linearLayout.addView(radioGroup)
+            radioGroupList.add(radioGroup)
+        }
+        return radioGroupList
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        results.clear()
+        radioGroupList.clear()
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            QuizFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ m9_quiz_localization/app/src/main/java/com/example/quizapplicationskillbox/WelcomeActivity.kt
@@ -0,0 +1,43 @@
+package com.example.quizapplicationskillbox
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quizapplicationskillbox.databinding.ActivityWelcomeBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+import java.util.*
+
+class WelcomeActivity : AppCompatActivity() {
+    val calendar = Calendar.getInstance()
+    val dateFormat = SimpleDateFormat("dd.MM.yyyy")
+    val locale = Locale.getDefault()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.skipButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.pickDateButton.setOnClickListener {
+            val dateDialog = MaterialDatePicker.Builder.datePicker()
+                .setTitleText(resources.getString(R.string.choose_your_birthday))
+                .build()
+            dateDialog.addOnPositiveButtonClickListener { timeInMillis ->
+                calendar.timeInMillis = timeInMillis
+                Snackbar.make(
+                    binding.pickDateButton,
+                    dateFormat.format(calendar.time),
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+            dateDialog.show(supportFragmentManager, "DatePicker")
+        }
+    }
+}
