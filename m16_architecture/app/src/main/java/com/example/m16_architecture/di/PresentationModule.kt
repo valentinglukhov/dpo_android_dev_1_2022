c6f93c628354745360b78100006b1b0669df9043
Валентин Глухов <glukhov_vy@vk.com>
2023-04-14 18:46:59 +0400

    Practice is done

--- m16_architecture/app/src/main/java/com/example/m16_architecture/di/PresentationModule.kt
+++ m16_architecture/app/src/main/java/com/example/m16_architecture/di/PresentationModule.kt
@@ -4,14 +4,17 @@ import com.example.m16_architecture.domain.GetUsefulActivityUseCase
 import com.example.m16_architecture.presentation.MainViewModel
 import com.example.m16_architecture.presentation.MainViewModelFactory
 import dagger.Module
+import dagger.Provides
 
 @Module
 class PresentationModule {
 
+    @Provides
     fun provideMainViewModel(getUsefulActivityUseCase: GetUsefulActivityUseCase): MainViewModel {
         return MainViewModel(getUsefulActivityUseCase)
     }
 
+    @Provides
     fun provideMainViewModelFactory(mainViewModel: MainViewModel): MainViewModelFactory {
         return MainViewModelFactory(mainViewModel)
     }
