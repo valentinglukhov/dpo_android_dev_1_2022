dbce65cba74ecec5d854dea5edcd5b5bb3af30a9
Валентин Глухов <glukhov_vy@vk.com>
2023-04-25 21:02:44 +0400

    Done

--- m18_permissions/app/src/main/java/com/example/m18_permissions/ui/main/PhotoFragment.kt
+++ m18_permissions/app/src/main/java/com/example/m18_permissions/ui/main/PhotoFragment.kt
@@ -29,9 +29,6 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.Executor
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss"
 
 class PhotoFragment : Fragment() {
@@ -66,16 +63,8 @@ class PhotoFragment : Fragment() {
             }
         }
     }
-
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
         executor = ContextCompat.getMainExecutor(requireContext())
         checkPermissions()
 
@@ -176,16 +165,5 @@ class PhotoFragment : Fragment() {
                 imageCapture
             )
         }, executor)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PhotoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
