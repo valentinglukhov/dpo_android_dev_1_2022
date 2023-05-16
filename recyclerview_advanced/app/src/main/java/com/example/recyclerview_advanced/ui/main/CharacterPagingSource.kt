062b9bdadb30b7dc3feba3c4a61a78d54b7c04fc
Валентин Глухов <glukhov_vy@vk.com>
2023-05-16 23:33:30 +0400

    Done

--- recyclerview_advanced/app/src/main/java/com/example/recyclerview_advanced/ui/main/CharacterPagingSource.kt
+++ recyclerview_advanced/app/src/main/java/com/example/recyclerview_advanced/ui/main/CharacterPagingSource.kt
@@ -3,10 +3,12 @@ package com.example.recyclerview_advanced.ui.main
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import retrofit2.HttpException
+import retrofit2.Response
 
 class CharacterPagingSource : PagingSource<Int, Character>() {
 
     private val repository: Repository = Repository()
+    private var response: Response<Results>? = null
 
     override fun getRefreshKey(state: PagingState<Int, Character>): Int? {
         val anchorPosition = state.anchorPosition ?: return null
@@ -17,17 +19,19 @@ class CharacterPagingSource : PagingSource<Int, Character>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> {
         val pageNumber = params.key ?: 1
         val pageSize = params.loadSize
-        val response = repository.getCharacterListFun(pageNumber)
 
         return try {
-            if (response.isSuccessful) {
-                val characters = response.body()?.results
+            response = repository.getCharacterListFun(pageNumber)
+            if (response?.isSuccessful == true) {
+                val characters = response!!.body()?.results
                 val nextPageNumber = if (characters?.isEmpty()!!) null else pageNumber + 1
                 val prevPageNumber = if (pageNumber > 1) pageNumber - 1 else null
                 LoadResult.Page(characters, prevPageNumber, nextPageNumber)
             } else {
-                LoadResult.Error(HttpException(response))
+                LoadResult.Error(HttpException(response!!))
             }
+        } catch (e: HttpException) {
+            LoadResult.Error(e)
         } catch (e: Exception) {
             LoadResult.Error(e)
         }
